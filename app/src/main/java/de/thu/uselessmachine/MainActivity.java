80c9cea6af254df9461e80135cc0dc46d522de30
bircni <nicolas@bircks.eu>
2023-01-12 14:52:03 +0100

    remove lines

--- app/src/main/java/de/thu/uselessmachine/MainActivity.java
+++ app/src/main/java/de/thu/uselessmachine/MainActivity.java
@@ -3,7 +3,6 @@ package de.thu.uselessmachine;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
