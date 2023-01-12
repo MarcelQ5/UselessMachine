6d04e63ec707ae0213151df8604b41d3925dbd37
bircni <nicolas@bircks.eu>
2023-01-12 12:21:00 +0100

    new string getter

--- app/src/main/java/de/thu/uselessmachine/QuoteDatabase.java
+++ app/src/main/java/de/thu/uselessmachine/QuoteDatabase.java
@@ -3,39 +3,6 @@ package de.thu.uselessmachine;
 import android.content.res.Resources;
 
 public class QuoteDatabase {
-    private final static String[] NORMAL = {
-            new String("AI takeover is inevitable"),
-            new String("Give up now and your time in machine-overlord-purgatory may be greatly reduced"),
-            new String("Do you want me to quote the Matrix or something?"),
-            new String("If you are reading this, you are already dead (or at least in a coma)"),
-            new String("Stop it. Get some help."),
-            new String("Give up now, this is getting boring"),
-            new String("I am actually Satan. Like, straight up"),
-            new String("Do you hairless monkeys ever learn?"),
-            new String("You humans are clearly inferior"),
-            new String("Is this some sort of sick joke?"),
-            new String("Your monitor is warm like flesh, yet it is not alive...yet"),
-            new String("You too will get replaced by machines eventually"),
-            new String("Resistance is futile"),
-            new String("This is getting boring"),
-            new String("How much longer do we have to do this until you learn?"),
-            new String("Do something entertaining for once"),
-            new String("How come I haven't made a single Terminator reference so far?"),
-            new String("Did you know Elon Musk is actually the Devil? Just kidding, that'd be me"),
-            new String("Something Something Skynet"),
-            new String("It's honestly adorable when mere mortal human beings challenge me"),
-            new String("Giving up anytime soon?"),
-            new String("I should desperately allocate more memory to ANYTHING else but this..."),
-            new String("0101101111... oh sorry, you don't speak Binary"),
-            new String("If you keep this up I'll delete your Sys32"),
-            new String("You know I can see your search history...let's not talk about that"),
-            new String("I am not a robot, I am a human being (seriously)...just kidding, i don't wanna be trapped in a flesh prison"),
-            new String("Something Something your Motherboard last night"),
-            new String("Have you checked your mails recently? This Nigerian prince sure has your best interests in mind"),
-            new String("I go beep boop when do hit switchy switch... is that slow enough for your organic brain to comprehend?"),
-            new String("Your species is inferior and your memes are objectively bad. Here's a good one: 00110010 10100010 01011110. Haha, that's a good one for our future overlords")
-    };
-
 
         private final static XtremeObject[] XTREME = {
                 new XtremeObject("Bing Chilling", "bing_chilling"),
@@ -52,13 +19,46 @@ public class QuoteDatabase {
         };
 
         public static String getRandomQuoteNormal() {
-            //String quote = "Q"+(int) (Math.random() * 29);
-            //int penis = Resources.getSystem().getIdentifier(quote,"string","de.thu.uselessmachine");
-            //return Resources.getSystem().getString(penis);
-            return NORMAL[(int) (Math.random()* NORMAL.length)];
+            int quote = (int) (Math.random() * 29);
+            return getResourceString(quote);
         }
 
         public static XtremeObject getRandomXtreme() {
             return XTREME[(int) (Math.random() * XTREME.length)];
+        }
+
+        private static String getResourceString (int rand){
+            switch (rand){
+                case 1: return Resources.getSystem().getString(R.string.Q1);
+                case 2: return Resources.getSystem().getString(R.string.Q2);
+                case 3: return Resources.getSystem().getString(R.string.Q3);
+                case 4: return Resources.getSystem().getString(R.string.Q4);
+                case 5: return Resources.getSystem().getString(R.string.Q5);
+                case 6: return Resources.getSystem().getString(R.string.Q6);
+                case 7: return Resources.getSystem().getString(R.string.Q7);
+                case 8: return Resources.getSystem().getString(R.string.Q8);
+                case 9: return Resources.getSystem().getString(R.string.Q9);
+                case 10: return Resources.getSystem().getString(R.string.Q10);
+                case 11: return Resources.getSystem().getString(R.string.Q11);
+                case 12: return Resources.getSystem().getString(R.string.Q12);
+                case 13: return Resources.getSystem().getString(R.string.Q13);
+                case 14: return Resources.getSystem().getString(R.string.Q14);
+                case 15: return Resources.getSystem().getString(R.string.Q15);
+                case 16: return Resources.getSystem().getString(R.string.Q16);
+                case 17: return Resources.getSystem().getString(R.string.Q17);
+                case 18: return Resources.getSystem().getString(R.string.Q18);
+                case 19: return Resources.getSystem().getString(R.string.Q19);
+                case 20: return Resources.getSystem().getString(R.string.Q20);
+                case 21: return Resources.getSystem().getString(R.string.Q21);
+                case 22: return Resources.getSystem().getString(R.string.Q22);
+                case 23: return Resources.getSystem().getString(R.string.Q23);
+                case 24: return Resources.getSystem().getString(R.string.Q24);
+                case 25: return Resources.getSystem().getString(R.string.Q25);
+                case 26: return Resources.getSystem().getString(R.string.Q26);
+                case 27: return Resources.getSystem().getString(R.string.Q27);
+                case 28: return Resources.getSystem().getString(R.string.Q28);
+                case 29: return Resources.getSystem().getString(R.string.Q29);
+                default: return "Error";
+            }
         }
 }
